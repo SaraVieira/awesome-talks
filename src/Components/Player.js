f3ea28661d220b3f24f5a538e5cb6ce1c9882379
Husni Munaya <husnimun@gmail.com>
2018-05-26 06:41:04 +0700

    Maintain Player component aspect ratio (#20)

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -7,12 +7,27 @@ import Favorite from './Favorite'
 import Play from './Styling/Play'
 import Watched from './Watched'
 
-const Video = styled.section`
+const Video = styled.div``
+
+const VideoWrapper = styled.section`
   position: relative;
   margin: auto;
 
   ${isNot('cinemaMode')`
-    height: ${remcalc(200)};
+    &:before {
+      display: block;
+      content: '';
+      width: 100%;
+      padding-top: 56.25%;
+    }
+
+    ${Video} {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+      }
   `};
 `
 
@@ -23,34 +38,29 @@ const Iframe = styled(YouTube)`
   transition: all 200ms ease;
   box-shadow: 0 ${remcalc(10)} ${remcalc(20)} rgba(0, 0, 0, 0.1),
     0 ${remcalc(6)} ${remcalc(6)} rgba(0, 0, 0, 0.12);
-  height: ${remcalc(200)};
+  height: 100%;
 
   ${is('cinemaMode')`
-        height: ${remcalc(600)};
+    height: ${remcalc(600)};
+    @media (max-width: ${remcalc(768)}) {
+      height: auto;
+    }
   `};
-
-  @media (max-width: ${remcalc(768)}) {
-    height: auto;
-  }
 `
 
 const Thumbnail = styled.img`
   display: block;
   width: 100%;
-  height: ${remcalc(200)};
+  height: 100%;
 `
 
 const Image = styled.div`
   position: relative;
   margin: auto;
-  height: ${remcalc(200)};
+  height: 100%;
   overflow: hidden;
   box-shadow: 0 ${remcalc(10)} ${remcalc(20)} rgba(0, 0, 0, 0.1),
     0 ${remcalc(6)} ${remcalc(6)} rgba(0, 0, 0, 0.12);
-
-  @media (max-width: ${remcalc(768)}) {
-    height: auto;
-  }
 
   ${is('cinemaMode')`
     height: auto;
@@ -58,29 +68,31 @@ const Image = styled.div`
 `
 
 export default ({ cinemaMode, id, link, showVideo, name, onClick, onEnd }) => (
-  <Video cinemaMode={cinemaMode}>
-    {showVideo || cinemaMode ? (
-      <Iframe
-        videoId={link}
-        id={`a-${link} do-not-delete-this-hack`}
-        onReady={e => e.target.playVideo()}
-        onEnd={onEnd}
-        cinemaMode={cinemaMode}
-        opts={{
-          width: '100%'
-        }}
-      />
-    ) : (
-      <Image cinemaMode={cinemaMode}>
-        <Play onClick={onClick} aria-label="Play Video" />
-        <Thumbnail
+  <VideoWrapper cinemaMode={cinemaMode}>
+    <Video cinemaMode={cinemaMode}>
+      {showVideo || cinemaMode ? (
+        <Iframe
+          videoId={link}
+          id={`a-${link} do-not-delete-this-hack`}
+          onReady={e => e.target.playVideo()}
+          onEnd={onEnd}
           cinemaMode={cinemaMode}
-          src={`https://img.youtube.com/vi/${link}/mqdefault.jpg`}
-          alt={name}
+          opts={{
+            width: '100%'
+          }}
         />
-      </Image>
-    )}
-    <Favorite id={id} />
-    <Watched id={id} />
-  </Video>
+      ) : (
+        <Image cinemaMode={cinemaMode}>
+          <Play onClick={onClick} aria-label="Play Video" />
+          <Thumbnail
+            cinemaMode={cinemaMode}
+            src={`https://img.youtube.com/vi/${link}/mqdefault.jpg`}
+            alt={name}
+          />
+        </Image>
+      )}
+      <Favorite id={id} />
+      <Watched id={id} />
+    </Video>
+  </VideoWrapper>
 )
