cdb9eac0c19bbcb0fd9993db06a56d8a3815714b
Sara Vieira <hey@iamsaravieira.com>
2018-05-26 23:02:01 +0100

    delete cyprus

--- src/Tests/Home.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-describe('My First Test', function() {
-  it('finds the content "type"', function() {
-    cy.visit('http://localhost:8080/')
-
-    cy.contains('h1.sc-bxivhb.gYIuKe')
-  })
-})
