128476196571662617044ee5d4350fc31ae0275e
Pavithra Kodmad <pksjce@gmail.com>
2018-05-27 13:26:45 +1000

    Only show carousel for more than 3 videos

--- src/Components/Slider/index.js
+++ src/Components/Slider/index.js
@@ -1,5 +1,9 @@
 import Carousel from 'nuka-carousel'
 
 export default props => {
-  return <Carousel {...props.settings}>{props.children}</Carousel>
+  const carouselSettings = {
+    slidesToShow: 3,
+    slidesToScroll: 2
+  }
+  return <Carousel {...carouselSettings}>{props.children}</Carousel>
 }
--- src/Pages/Tags.js
+++ src/Pages/Tags.js
@@ -13,26 +13,25 @@ export default () => (
     <Header title="Categories" noSearch />
     <Query query={TAGS}>
       {({ data: { allTagses } }) => {
-        const carouselSettings = {
-          slidesToShow: 4,
-          slidesToScroll: 2
-        }
         return (
           <div>
-            {allTagses.map(s => (
-              <div key={s.id}>
-                <Item key={s.id} href={makeLink(s.name)}>
-                  {s.name}
-                </Item>
-                <Row>
-                  <Slider settings={carouselSettings}>
-                    {s.videos.map(v => {
-                      return <Video key={v.id} speaker={v.speaker} {...v} />
-                    })}
-                  </Slider>
-                </Row>
-              </div>
-            ))}
+            {allTagses.map(s => {
+              const RowWrapper = s.videos.length > 3 ? Slider : Row
+              return (
+                <div key={s.id}>
+                  <Item key={s.id} href={makeLink(s.name)}>
+                    {s.name}
+                  </Item>
+                  <Row>
+                    <RowWrapper>
+                      {s.videos.map(v => {
+                        return <Video key={v.id} speaker={v.speaker} {...v} />
+                      })}
+                    </RowWrapper>
+                  </Row>
+                </div>
+              )
+            })}
           </div>
         )
       }}
