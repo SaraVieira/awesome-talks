97d29164270043caa844f93c2fb3110d6ce8691a
pkodmad <pkodmad@atlassian.com>
2018-05-28 23:13:10 +1000

    Make the video tags and name look a little better

--- src/Components/Styling/Tag.js
+++ src/Components/Styling/Tag.js
@@ -6,16 +6,16 @@ export default styled(Link)`
   opacity: 1;
   border: none;
   opacity: 0.8;
-  font-weight: 600;
-  color: ${props => props.theme.black};
-  margin-top: ${remcalc(-20)};
+  font-weight: 500;
+  color: ${props => props.theme.darkGrey};
+  margin-top: ${remcalc(-10)};
   padding: 0;
   margin-bottom: ${remcalc(10)};
   margin-right: ${remcalc(10)};
 
   &:hover {
     opacity: 1;
-    color: ${props => props.theme.black};
+    color: ${props => props.theme.darkGrey};
   }
   &:after {
     display: none;
