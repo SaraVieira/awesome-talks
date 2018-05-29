a7f4a4419a8907d8c8ba461c7b13118e57243db1
Sara Vieira <hey@iamsaravieira.com>
2018-05-29 16:49:19 +0100

    add helmet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Home from './Pages/Home'
 import Speakers from './Pages/Speakers'
 import Speaker from './Pages/Speaker'
@@ -8,12 +8,12 @@ import Tag from './Pages/Tag'
 import Favorites from './Pages/Favorites'
 
 export default () => (
-    <div>
+    <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/favorites" component={Favorites} />
-        <Route path="/categories" component={Tags} />
-        <Route path="/category/:category" component={Tag} />
-        <Route path="/speakers" component={Speakers} />
-        <Route path="/speaker/:speaker" component={Speaker} />
-    </div>
+        <Route exact path="/favorites" component={Favorites} />
+        <Route exact path="/categories" component={Tags} />
+        <Route exact path="/category/:category" component={Tag} />
+        <Route exact path="/speakers" component={Speakers} />
+        <Route exact path="/speaker/:speaker" component={Speaker} />
+    </Switch>
 )
--- src/server.js
+++ src/server.js
@@ -1,53 +1,14 @@
-import App from './Pages/Home'
+import App from './App'
 import React from 'react'
 import express from 'express'
 import { renderToString } from 'react-dom/server'
 import { ServerStyleSheet, ThemeProvider } from 'styled-components'
 import { getDataFromTree, ApolloProvider } from 'react-apollo'
+import { Helmet } from 'react-helmet'
 import { StaticRouter } from 'react-router'
 import 'isomorphic-fetch'
 import client from './Utils/stateLink'
 import theme from './Utils/theme'
-
-// const context = {}
-// const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
-
-// const server = express()
-// server
-//   .disable('x-powered-by')
-//   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-//   .get('/*', async (req, res) => {
-//     const customRenderer = node => {
-//       const App = (
-//         <ApolloProvider client={client}>
-//           <StaticRouter location={req.url} context={context}>
-//             <ThemeProvider theme={theme}>{node}</ThemeProvider>
-//           </StaticRouter>
-//         </ApolloProvider>
-//       )
-//       return getDataFromTree(App).then(() => {
-//         const initialApolloState = client.extract()
-//         const html = renderToString(App)
-//         return { html, initialApolloState }
-//       })
-//     }
-
-//     // try {
-//     const html = await render({
-//       req,
-//       res,
-//       routes,
-//       assets,
-//       customRenderer,
-//       document: Document
-//     })
-//     res.send(html)
-//     // } catch (error) {
-//     //   res.json(error)
-//     // }
-//   })
-
-// export default server
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
 
@@ -74,14 +35,19 @@ server
                     </StaticRouter>
                 )
             )
+
             // Generate all the style tags so they can be rendered into the page
             const styleTags = sheet.getStyleTags()
+            const helmet = Helmet.renderStatic()
 
             res.send(
                 // prettier-ignore
                 `<!doctype html>
-    <html lang="">
+    <html lang="" ${helmet.htmlAttributes.toString()}>
     <head>
+        ${helmet.title.toString()}
+        ${helmet.meta.toString()}
+        ${helmet.link.toString()}
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charSet='utf-8' />
         <meta name="viewport" content="width=device-width, initial-scale=1">
