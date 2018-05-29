670022b5d7b62afff5126cf68c897065395808e5
Sara Vieira <hey@iamsaravieira.com>
2018-05-29 14:38:32 +0100

    add router and apollo

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import { renderToString } from 'react-dom/server'
 import { ServerStyleSheet } from 'styled-components'
 import { getDataFromTree } from 'react-apollo'
+import { StaticRouter } from 'react-router'
 import { HttpLink } from 'apollo-link-http'
 import { ApolloClient } from 'apollo-client'
 import { ApolloLink } from 'apollo-link'
@@ -66,6 +67,7 @@ const client = new ApolloClient({
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST)
 
 const server = express()
+const context = {}
 
 server
     .disable('x-powered-by')
@@ -76,41 +78,49 @@ server
             // Create the server side style sheet
             const sheet = new ServerStyleSheet()
             // When the app is rendered collect the styles that are used inside it
-            const markup = renderToString(sheet.collectStyles(<App />))
+            const markup = renderToString(
+                sheet.collectStyles(
+                    <StaticRouter location={req.url} context={context}>
+                        <App />
+                    </StaticRouter>
+                )
+            )
+            console.log(markup)
             // Generate all the style tags so they can be rendered into the page
             const styleTags = sheet.getStyleTags()
 
-            res.send(`<!doctype html>
+            res.send(
+                // prettier-ignore
+                `<!doctype html>
     <html lang="">
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charSet='utf-8' />
-        <title>Welcome to Razzle</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${
-            assets.client.css
-                ? `<link rel="stylesheet" href="${assets.client.css}">`
-                : ''
-        }
-<div id="root"></div>
+    assets.client.css
+        ? `<link rel="stylesheet" href="${assets.client.css}">`
+        : ''
+}
         ${
-            process.env.NODE_ENV === 'production'
-                ? `<script src="${assets.client.js}" defer></script>`
-                : `<script src="${
-                      assets.client.js
-                  }" defer crossorigin></script>`
-        }
+    process.env.NODE_ENV === 'production'
+        ? `<script src="${assets.client.js}" defer></script>`
+        : `<script src="${assets.client.js}" defer crossorigin></script>`
+}
         <!-- Render the style tags gathered from the components into the DOM -->
         ${styleTags}
     </head>
     <body>
         <div id="root">${markup}</div>
-                <script>
+           <script>
           window.__APOLLO_STATE__ = ${JSON.stringify(
-              initialApolloState
-          ).replace(/</g, '\\u003c')}
+        initialApolloState
+    ).replace(/</g, '\\u003c')}
         </script>
     </body>
-</html>`)
+</html>`
+            )
         })
     })
+
+export default server
