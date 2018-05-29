36641365b92c8eb696112b86602f55fe02a01569
Hosmel Quintana <hosmelq@gmail.com>
2018-05-29 16:31:00 -0600

    fix favorites page (#46)
    * fix favorites page
    
    * optimize load

--- src/Pages/Favorites.js
+++ src/Pages/Favorites.js
@@ -3,7 +3,7 @@ import { Col, Row, Grid } from 'react-styled-flexboxgrid'
 import Query from './../Components/Query'
 import Video from './../Components/Video'
 import GET_FAVORITES from '../Queries/GET_FAVORITES'
-import GET_VIDEO from '../Queries/GET_VIDEO'
+import GET_VIDEOS from '../Queries/GET_VIDEOS'
 
 export default () => (
   <Grid>
@@ -12,13 +12,11 @@ export default () => (
       <Col xs={12}>
         <Query query={GET_FAVORITES}>
           {({ data: { favorites } }) => (
-            <Row>
-              {favorites.map(id => (
-                <Query key={id} query={GET_VIDEO} variables={{ id }}>
-                  {({ data: { Videos: v } }) => <Video key={v.id} {...v} />}
-                </Query>
-              ))}
-            </Row>
+            <Query query={GET_VIDEOS} variables={{ ids: favorites }}>
+              {({ data: { allVideoses } }) => (
+                <Row>{allVideoses.map(v => <Video key={v.id} talk={v} />)}</Row>
+              )}
+            </Query>
           )}
         </Query>
       </Col>
