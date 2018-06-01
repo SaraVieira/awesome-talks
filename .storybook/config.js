ba874d752a248b2b42c6e4f4ce8721304fa0624c
Sara Vieira <hey@iamsaravieira.com>
2018-06-01 18:00:55 +0200

    Revert "fix styles in storybook"
    This reverts commit 6a0238706e736190df87e71c2fc20f44b5f96701.

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,6 @@ import { configure } from '@storybook/react'
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.story.js$/)
 function loadStories() {
-    require('../src/Utils/global-styles')
     req.keys().forEach(filename => req(filename))
 }
 
