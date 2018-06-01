6a0238706e736190df87e71c2fc20f44b5f96701
Sara Vieira <hey@iamsaravieira.com>
2018-06-01 17:55:56 +0200

    fix styles in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import { configure } from '@storybook/react'
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.story.js$/)
 function loadStories() {
+    require('../src/Utils/global-styles')
     req.keys().forEach(filename => req(filename))
 }
 
