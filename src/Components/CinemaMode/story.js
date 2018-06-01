66eae42cd27bab0788f4729075ed4770e58c3cbe
pkodmad <pkodmad@atlassian.com>
2018-06-01 15:39:33 +1000

    Add more storybooks

---
+++ src/Components/CinemaMode/story.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import CinemaView from './index'
+
+storiesOf('CinemaView', module).add('Default', () => {
+    return <CinemaView />
+})
