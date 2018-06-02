10603e68e6ca61a630e19d7a2b2339d05fe26afb
Vijay Singh <sudovijay@gmail.com>
2018-06-02 21:44:11 +0530

    missin weight

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -77,7 +77,7 @@ injectGlobal`
 `
 
 const Name = styled.h2`
-    font-size: 400;
+    font-weight: 400;
     font-size: ${remcalc(18)};
     color: ${props => props.theme.black};
     letter-spacing: ${remcalc(-0.63)};
