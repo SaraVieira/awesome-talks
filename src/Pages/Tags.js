0e85921674ecb15e10e926a8535825d38a4476da
Sara Vieira <hey@iamsaravieira.com>
2018-06-05 16:11:22 +0100

    improvemnts to categories

--- src/Pages/Tags.js
+++ src/Pages/Tags.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Header from './../Components/Header'
 import { Col, Row, Grid } from 'react-styled-flexboxgrid'
 import Query from './../Components/Query'
-import Item from './../Components/Styling/Item'
+import { graphql } from 'react-apollo'
+
 import TAGS from '../Queries/TAGS'
 import Nav from './../Components/Nav'
 import { Helmet } from 'react-helmet'
+import Filter from '../Utils/search'
+import GET__TAGS_SEARCH from '../Queries/GET__TAGS_SEARCH'
+import Tag from '../Components/Tag'
 
-const makeLink = name => `/category/${name.replace(/\s+/g, '-').toLowerCase()}`
-
-export default () => (
+const Tags = ({ data: { searchTags } }) => (
     <Grid>
         <Helmet>
             <title>Awesome Talks - Categories</title>
@@ -30,26 +32,29 @@ export default () => (
         </Helmet>
         <div role="banner">
             <Nav />
-            <Header title="Categories" noSearch />
+            <Header
+                title="Categories"
+                code
+                query={GET__TAGS_SEARCH}
+                keyName="searchTags"
+            />
         </div>
         <Row>
             <Col xs={12}>
                 <main>
                     <Query query={TAGS}>
-                        {({ data: { allTagses } }) => {
-                            return (
-                                <Row>
-                                    {allTagses.map(s => (
-                                        <Item key={s.id} to={makeLink(s.name)}>
-                                            {s.name}
-                                        </Item>
-                                    ))}
-                                </Row>
-                            )
-                        }}
+                        {({ data: { allTagses: allTags } }) => (
+                            <Row style={{ justifyContent: 'space-around' }}>
+                                {Filter(searchTags, allTags).map(t => (
+                                    <Tag key={t.id} {...t} />
+                                ))}
+                            </Row>
+                        )}
                     </Query>
                 </main>
             </Col>
         </Row>
     </Grid>
 )
+
+export default graphql(GET__TAGS_SEARCH)(Tags)
