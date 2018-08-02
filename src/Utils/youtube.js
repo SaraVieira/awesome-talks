17dfe0c8d9db4bf3d59325e615e4fe01c66c1921
Sara Vieira <saravieira@Saras-MacBook-Pro.local>
2018-08-02 21:51:13 +0100

    commas

--- src/Utils/youtube.js
+++ src/Utils/youtube.js
@@ -5,13 +5,10 @@ export const getDuration = duration => {
     const date = new Date(null)
     date.setSeconds(duration)
     if (duration > 3600) {
-    // We use addMinutes instead of subMinutes because 
-    // Date.prototype.getTimezoneOffset() returns negative number for positive offsets.
-    return format(
-      addMinutes(date, date.getTimezoneOffset()), 
-      'H[h][ ]m[m]'
-    )
-  }
+        // We use addMinutes instead of subMinutes because
+        // Date.prototype.getTimezoneOffset() returns negative number for positive offsets.
+        return format(addMinutes(date, date.getTimezoneOffset()), 'H[h][ ]m[m]')
+    }
 
     return format(date, 'm[m]')
 }
